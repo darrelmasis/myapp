61bf3f05c007aced4f911bcca1830b97dc8f011f
Darrelmasis <darrelmasis@gmail.com>
2021-08-22 21:12:40 -0600

    La consulta ahora se hace desde el archivo socket.io.js

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,16 +1,3 @@
-const connection = require('../connection')
-const userModel = require('../models/base_model')
-
-
-exports.read = (req, res, next) => {
-  const query = 'SELECT * FROM customers WHERE MATCH(fullName,bussinessName) AGAINST("rosa margarita") LIMIT 5'
-  connection.query(query, (error, result) => {
-    if (error) {
-      console.log('Ha ocurrido un error en la consulta: ' + error)
-    } else {
-      const x = new userModel('Darrel Masis')
-      x.hello('Darrel I. Masis')
-      res.render('index', { users: result })
-    }
-  })
+module.exports.read = (req, res) => {
+  res.render('index', req.body)
 }
--- src/models/base_model.js
+++ src/models/base_model.js
@@ -1,9 +1,28 @@
-module.exports = class User {
-  constructor(t) {
-    this.title = t
+const connection = require("../connection")
+class Customer {
+  constructor() {
+    this.result = {}
   }
+  /**
+   * 
+   * @param {string} table table name
+   * @param {*} condition [WHERE] + column = something
+   */
+  read(table, value) {
+    let mode = 'like'
+    const searchValue = value.split('').lenght
+    const fulltext = `SELECT * FROM ${table} WHERE MATCH(fullName) AGAINST('${value}')`
+    const like = `SELECT * FROM ${table} WHERE fullName LIKE '%${value}%'`
+    let query = like
 
-  hello(name) {
-    console.log('Hola ' + name)
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(`Ha ocurrido un error en la consulta: ${err}`)
+      } else {
+        return result
+      }
+    })
   }
 }
+
+module.exports = Customer
