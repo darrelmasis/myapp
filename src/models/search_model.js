b90596655005dd6246d5a875e4903e9af6809d3c
Darrel Masis <darrelmasis@gmail.com>
2021-08-30 14:52:53 -0600

    Agrega búsqueda por código de cliente

--- src/models/search_model.js
+++ src/models/search_model.js
@@ -3,7 +3,7 @@ const baseModel = require('./base_model')
 class Search {
 
   search(value) {
-   return baseModel.read('*','customers', `WHERE fullName LIKE '%${value}%'`)
+   return baseModel.read('*','customers', `WHERE fullName LIKE '%${value}%' OR customerCode LIKE '%${value}%'`)
   }
 }
 
