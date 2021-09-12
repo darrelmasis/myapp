38b4e5273c7b23f0cc722712446df23dc9aee20b
Darrel Masis <darrelmasis@gmail.com>
2021-09-11 22:04:32 -0600

    Hace uso de la función nomProp y refactoriza los controladores

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -5,29 +5,37 @@ const { promisify } = require('util')
 let response = {}
 
 const signup = async (req, res) => {
-  try { 
+  try {
     const user = {
       fullName: req.body.fullName,
-      username: req.body.username,
-      email: req.body.email,
+      username: req.body.username.toLowerCase(),
+      email: req.body.email.toLowerCase(),
       password: await bcryptjs.hash(req.body.password, 8),
       gender: req.body.gender,
       createTime: new Date()
     }
-    if(user.fullName === '' || user.username === '' || user.email === '' || user.password === '' || user.gender === '') {
+    const exist = await userModel.exist(user.email)
+    if (exist) {
       response.type = 'error'
-      response.message = 'Todos los campos son obligatorios'
+      response.message = `El correo <span class="fw-bold">${user.email}</span> ya está asociado a una cuenta`
       return res.send(response)
     } else {
-      if(req.body.password === '') {
+
+      if (user.fullName === '' || user.username === '' || user.email === '' || user.password === '' || user.gender === '') {
         response.type = 'error'
-        response.message = 'Las contraseñas no coinciden'
+        response.message = 'Todos los campos son obligatorios'
+        return res.send(response)
+      } else {
+        if (req.body.password === '') {
+          response.type = 'error'
+          response.message = 'Las contraseñas no coinciden'
+          return res.send(response)
+        }
+        userModel.create(user)
+        response.type = 'success'
+        response.message = 'Usuario registrado correctamente'
         return res.send(response)
       }
-      userModel.create(user)
-      response.type = 'success'
-      response.message = 'Usuario registrado correctamente'
-      return res.send(response)
     }
 
   } catch (error) {
@@ -41,7 +49,7 @@ const signup = async (req, res) => {
 const signin = async (req, res) => {
   try {
     const user = {
-      username: req.body.username || '',
+      username: req.body.username,
       password: req.body.password
     }
 
@@ -50,14 +58,20 @@ const signin = async (req, res) => {
       response.message = 'Todos los campos son obligatorios'
       return res.send(response)
     } else {
-      const results = await userModel.signin(user)
-      if (results.length > 0) {
-        if (!(await bcryptjs.compare(user.password, results[0].password))) {
+      const exist = await userModel.exist(user.username)
+      if (!exist) {
+        response.type = 'error'
+        response.message = `El usuario <span class="fw-bold">${user.username}</span> no está asociado a ninguna cuenta`
+        return res.send(response)
+      } else {
+        const results = await userModel.signin(user)
+        console.log(results)
+        if (!(await bcryptjs.compare(user.password, results.password))) {
           response.type = 'error'
           response.message = 'Usuario o contraseña inconrrecto'
           return res.send(response)
         } else {
-          const id = results[0].id
+          const id = results.id
           const token = jwt.sign({ id: id }, 'super_secret', { expiresIn: '7d' })
           const cookiesOptions = {
             expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
@@ -68,10 +82,6 @@ const signin = async (req, res) => {
           response.message = 'Iniciando sesión...'
           return res.send(response)
         }
-      } else {
-        response.type = 'error'
-        response.message = `El usuario <span class="fw-bold">${user.username}</span> no está asociado a ninguna cuenta`
-        return res.send(response)
       }
     }
 
@@ -79,7 +89,7 @@ const signin = async (req, res) => {
     console.log('Error: ' + error)
     response.type = 'error'
     response.message = '¡Oops! Hubo algunos errores al iniciar sesión'
-    return response
+    return res.send(response)
   }
 }
 
@@ -88,8 +98,8 @@ const isLogged = async (req, res, next) => {
     try {
       const id = await promisify(jwt.verify)(req.cookies.jwt, 'super_secret')
       userModel.isLogged(id.id).then(data => {
-        data[0].firstname = data[0].fullName.split(' ')[0]
-        req.user = data[0]
+        data.firstname = data.fullName.split(' ')[0]
+        req.user = data
         req.isLogged = true
         return next()
       })
@@ -107,18 +117,18 @@ const isLogged = async (req, res, next) => {
 const get = async (req, res, next) => {
   try {
     await userModel.read(req.params.username)
-    .then(data => {
-      console.log(data)
-      if (data.length === 0) {
-        req.user = false
-        return next()
-      } else {
-        req.user = data[0]
-        return next()
-      } 
-    })
-  } catch {
-
+      .then(data => {
+        console.log(data)
+        if (data.length === 0) {
+          req.user = false
+          return next()
+        } else {
+          req.user = data
+          return next()
+        }
+      })
+  } catch (error) {
+    return error
   }
 }
 
