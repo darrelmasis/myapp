61e7a04ed8c1a96465fa4bd7dea0c830b0b06be7
Darrel Masis <darrelmasis@gmail.com>
2021-09-11 22:12:31 -0600

    Agrega sentencias try - catch, refactoriza el método signin y agregá el método exist

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -2,21 +2,47 @@ const baseModel = require('./base_model')
 
 class User {
 
-    async create(data) {
-        return await baseModel.create('users', data)
+  async create(data) {
+    try {
+      return await baseModel.create('users', data)
+    } catch (error) {
+      return error
     }
+  }
 
-    async read(data) {
-        return await baseModel.read('*', 'users', `WHERE username = '${data}'`)
+  async read(data) {
+    try {
+      return await baseModel.read('*', 'users', `WHERE username = '${data}'`)
+    } catch (error) {
+      return error
     }
+  }
 
-    async signin(data) {
-        return await baseModel.read('*', 'users', `WHERE username = '${data.username}' OR email = '${data.username}'`)
+  async signin(data) {
+    try {
+      return await baseModel.read('*', 'users', 'WHERE username = ? OR email = ?', [data.username, data.username])
+    } catch (error) {
+      return error
     }
+  }
 
-    async isLogged(id) {
-        return await baseModel.read('*', 'users', `WHERE id = '${id}'`)
+  async isLogged(id) {
+    try {
+      return await baseModel.read('*', 'users', `WHERE id = '${id}'`)
+    } catch (error) {
+      return error
     }
+  }
+
+  async exist(data) {
+    try {
+      const result = await baseModel.read('email', 'users', 'WHERE username = ? OR email = ?', [data, data])
+      if (result) { return true }
+      return false
+    } catch (error) {
+      return error
+    }
+  }
 }
 
 module.exports = new User
