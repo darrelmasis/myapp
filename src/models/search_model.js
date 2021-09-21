0246b7da4053129e09c8491f99820da5f4c1c00a
Darrelmasis <darrelmasis@gmail.com>
2021-09-21 07:16:43 -0600

    Agrega búsqueda con match

--- src/models/search_model.js
+++ src/models/search_model.js
@@ -2,9 +2,19 @@ const baseModel = require('./base_model')
 
 class Search {
 
-  async search(value) {
+  async search(value, mode) {
     try {
-      return await baseModel.read('*','customers', 'WHERE fullName LIKE ? OR customerCode LIKE ?', [`%${value}%`,`%${value}%`])
+      switch (mode) {
+        case 'match':
+          return await baseModel.read('*', 'customers', 'WHERE MATCH(fullName) AGAINST(? IN NATURAL LANGUAGE MODE)', [`${value}`])
+          break;
+        case 'like':
+          return await baseModel.read('*', 'customers', 'WHERE fullName LIKE ? OR customerCode LIKE ?', [`%${value}%`, `%${value}%`])
+          break;
+        default:
+          return await baseModel.read('*', 'customers', 'WHERE fullName LIKE ? OR customerCode LIKE ?', [`%${value}%`, `%${value}%`])
+          break;
+      }
     } catch (error) {
       return error
     }
