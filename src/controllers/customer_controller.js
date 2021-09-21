653f2e0c22e0530ae2e38487c877884ba0a379cd
Darrelmasis <darrelmasis@gmail.com>
2021-09-21 07:09:41 -0600

    Elimina la consulta para el nombre del vendedor

--- src/controllers/customer_controller.js
+++ src/controllers/customer_controller.js
@@ -2,31 +2,35 @@ const customer_model = require('../models/customer_model')
 
 const get = async (req, res, next) => {
   try {
-    await customer_model.get(req.params.id).then( async data => {
-      req.customer = data[0]
-      await customer_model.getSeller(data[0].seller).then(dataSeller => {
-        data[0].sellerInfo = dataSeller[0]
-        data[0].dateFormat = d => {
-          const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-          const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
-          let date = new Date(d)
-          let day = date.getDate()
-          day < 10 ? day = '0' + day : null
-          let month = date.getUTCMonth()
-          month < 10 ? month = '0' + month : null
-          let year = date.getFullYear()
+    await customer_model.get(req.params.id)
+      .then(data => {
+        if (data.length === 0) {
+          res.customerData = false
+          return next()
 
-          return {
-            day: day,
-            month: month,
-            year: year,
-            short: day + '/' + month + '/' + year,
-            large: days[date.getDay()] + ' ' + day + ' de ' + months[date.getMonth()] + ' de  ' + year
+        } else {
+          res.customerData = data[0]
+          data[0].dateFormat = d => {
+            const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+            const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+            let date = new Date(d)
+            let day = date.getDate()
+            day < 10 ? day = '0' + day : null
+            let month = date.getUTCMonth()
+            month < 10 ? month = '0' + month : null
+            let year = date.getFullYear()
+
+            return {
+              day: day,
+              month: month,
+              year: year,
+              short: day + '/' + month + '/' + year,
+              large: days[date.getDay()] + ' ' + day + ' de ' + months[date.getMonth()] + ' de  ' + year
+            }
           }
+          return next()
         }
       })
-      return next()
-    })
   } catch (error) {
     console.log(error)
   }
