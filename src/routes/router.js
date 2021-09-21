200af868d04a4f72bd68b24cd41b0d4e4847c61c
Darrelmasis <darrelmasis@gmail.com>
2021-09-21 07:26:25 -0600

    Agrega validación y redireccionamiento

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,28 +5,47 @@ const userController = require('../controllers/user_controller')
 const customerController = require('../controllers/customer_controller')
 const searchController = require('../controllers/search_controller')
 
-router.get('/',userController.isLogged, (req, res) => {
-  if (req.isLogged) {
-    res.render('index', { user: req.user })
+router.get('/', userController.isLogged, (req, res) => {
+  if (res.isLogged) {
+    res.render('index', { user: res.data })
   } else {
     res.redirect('/signin')
   }
 })
 
-router.get('/@:username',userController.isLogged, userController.get, (req, res) => {
-  if(req.user === false) {
-    res.status('404').render('404')
+router.get('/@:username', userController.isLogged, userController.get, (req, res) => {
+  // si está logueado
+  // si el perfil es el del usuario logueado
+  if (res.isLogged) {
+    if (res.userProfile === false) {
+      res.status(404).render('404')
+    } else {
+      if (res.userProfile.username === res.data.username) {
+        res.userProfile.active = true
+      } else {
+        res.userProfile.active = false
+      }
+      res.render('userProfile', { userProfile: res.userProfile, user: res.data })
+    }
   } else {
-    res.send('Perfil de usuario de: ' + req.user.fullName)
+    res.redirect('/signin')
   }
 })
 
 router.get('/cliente/:id', userController.isLogged, customerController.get, (req, res) => {
-  res.render('profile', {customer: req.customer, user: req.user})
+  if (res.isLogged) {
+    if (res.customerData === false) {
+      res.status(404).render('404')
+    } else {
+      res.render('customerProfile', { customer: res.customerData, user: res.data })
+    }
+  } else {
+    res.redirect('/signin')
+  }
 })
 
 // Rutas del registro de usuarios
-router.get('/signup',userController.isLogged,(req, res) => {
+router.get('/signup', userController.isLogged, (req, res) => {
   if (req.isLogged) {
     res.redirect('/')
   } else {
@@ -37,7 +56,7 @@ router.post('/signup', userController.signup)
 
 // Rutas del inicio de sesión de usuarios
 router.get('/signin', userController.isLogged, (req, res) => {
-  if(req.isLogged) {
+  if (req.isLogged) {
     res.redirect('/')
   } else {
     res.render('signin')
