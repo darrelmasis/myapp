7b8b601702599585df81364efed599eb49c322b1
Darrel Masis <darrelmasis@gmail.com>
2022-08-25 16:58:40 -0600

    Agrega la propiedad connectTimeout en las configuraciones de la conección

--- src/config.js
+++ src/config.js
@@ -6,6 +6,7 @@ module.exports = {
       user: 'b574b9c8ca56c8',
       password: 'f962c8c8',
       database: 'heroku_56689a12f8a9ca9',
+      connectTimeout: 30000 //10000 default
     },
     local: {
       host: 'localhost',
