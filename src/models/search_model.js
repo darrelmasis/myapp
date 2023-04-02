b41bb94b5bfcc6662023b876a8a389597403263d
Darrel Masis <darrelmasis@gmail.com>
2023-04-02 14:23:46 -0600

    Agreg un valor por defecto al parametro scrollLimit del método search

--- src/models/search_model.js
+++ src/models/search_model.js
@@ -7,11 +7,11 @@ const baseModel = require('./base_model')
  */
 class Search {
 
-  async search(value, scrollLimit) {
+  async search(value, scrollLimit = {start: 0, end: 9}) {
     try {
-          let customers = await baseModel.read('customers.id, customers.customerCode AS href, customers.fullName, customers.avatar, "cliente/" AS type, sellers.sellerFullName AS description, CASE WHEN fullName LIKE ? THEN 1 WHEN fullName LIKE ? THEN 2 WHEN fullName LIKE ? THEN 3 ELSE 9999 END AS Sort', 'customers', 'LEFT JOIN sellers ON customers.seller = sellers.sellerCode WHERE MATCH(fullName) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(customerCode) AGAINST(? IN NATURAL LANGUAGE MODE) OR fullName LIKE ? OR customerCode LIKE ? ORDER BY Sort ASC LIMIT ?, ?', [`%${value}`, `${value}%`, `%${value}%`, `${value}`, `${value}`,`%${value}%`,`%${value}%`, scrollLimit.start, scrollLimit.end])
-
-          let users = await baseModel.read('id, username AS href, fullName, "@" AS type, avatar, bio AS description, CASE WHEN fullName LIKE ? THEN 1 WHEN fullName LIKE ? THEN 2 WHEN fullName LIKE ? THEN 3 ELSE 9999 END AS Sort', 'users', 'WHERE MATCH(fullName) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(username) AGAINST(? IN NATURAL LANGUAGE MODE) OR fullName LIKE ? ORDER BY Sort ASC LIMIT ?, ?', [`%${value}`, `${value}%`, `%${value}%`, `${value}`, `${value}`, `%${value}%`, scrollLimit.start, scrollLimit.end])
+          let customers = await baseModel.read('customers.id, customers.customerCode AS href, customers.fullName, customers.avatar, "cliente/" AS type, sellers.sellerFullName AS description, CASE WHEN fullName LIKE ? THEN 1 WHEN fullName LIKE ? THEN 2 WHEN fullName LIKE ? THEN 3 ELSE 9999 END AS Sort', 'customers', 'LEFT JOIN sellers ON customers.seller = sellers.sellerCode WHERE MATCH(fullName) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(customerCode) AGAINST(? IN NATURAL LANGUAGE MODE) OR fullName LIKE ? OR customerCode LIKE ? ORDER BY Sort ASC LIMIT ?, ?', [`%${value}`, `${value}%`, `%${value}%`, `${value}`, `${value}`, `%${value}%`, `%${value}%`, scrollLimit.start, scrollLimit.end])
+      
+      let users = await baseModel.read('id, username AS href, fullName, "@" AS type, avatar, bio AS description, CASE WHEN fullName LIKE ? THEN 1 WHEN fullName LIKE ? THEN 2 WHEN fullName LIKE ? THEN 3 ELSE 9999 END AS Sort', 'users', 'WHERE MATCH(fullName) AGAINST(? IN NATURAL LANGUAGE MODE) OR MATCH(username) AGAINST(? IN NATURAL LANGUAGE MODE) OR fullName LIKE ? ORDER BY Sort ASC LIMIT ?, ?', [`%${value}`, `${value}%`, `%${value}%`, `${value}`, `${value}`, `%${value}%`, scrollLimit.start, scrollLimit.end])
 
           users[0] != undefined ? customers.push(users[0]) : false
 
